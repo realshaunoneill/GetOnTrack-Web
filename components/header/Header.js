21b0f9b4d197420cb024f26e02e4d025cbeed9a0
XeliteXirish <shaunoneill7@gmail.com>
2020-01-08 01:42:40 +0000

    Footer

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Navbar from "./Navbar";
-import ServiceBox from "./ServiceBox";
 
 const Header = () => (
-    <section className="hero is-fullheight styledBackground">
+    <section className="hero is-large styledBackground">
         <div className="hero-head">
             <Navbar/>
         </div>
@@ -16,16 +15,6 @@ const Header = () => (
                     <h2 className="header-subtitle">
                         &bull;&nbsp;Your premium resource for public transport information
                     </h2>
-                </div>
-            </div>
-        </div>
-        <div className="hero-foot">
-            <div className="container has-text-centered">
-                <div className="columns">
-                    <ServiceBox title="Dublin Bus" type="dublinBus"/>
-                    <ServiceBox title="Irish Rail" type="irishRail"/>
-                    <ServiceBox title="Luas" type="luas"/>
-                    <ServiceBox title="Dublin Bikes" type="dublinBikes"/>
                 </div>
             </div>
         </div>
--- components/searchBox/SearchBox.js
+++ components/searchBox/SearchBox.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ServiceBox from "../header/ServiceBox";
 
-const SearchBox = () => (
-    <div className="searchBox hero is-medium styledBackgroundReverse">
-        <div className="hero-body has-text-centered">
-            <div className="field">
-                <input className="input" type="text"/>
+const SearchBox = () => {
+    const [currentMode, setCurrentMode] = useState("dublinBus");
+
+    return (
+        <div className="searchBox hero is-medium styledBackgroundReverse">
+            <div className="hero-head">
+                <div className="container has-text-centered">
+                    <div className="columns">
+                        <ServiceBox title="Dublin Bus" type="dublinBus"/>
+                        <ServiceBox title="Irish Rail" type="irishRail"/>
+                        <ServiceBox title="Luas" type="luas"/>
+                        <ServiceBox title="Dublin Bikes" type="dublinBikes"/>
+                    </div>
+                </div>
+            </div>
+            <div className="hero-body has-text-centered">
+                <div className="field">
+                    <input className="input" type="text"/>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default SearchBox;
