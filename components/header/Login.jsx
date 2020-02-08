58912f9b5dbe91627a8b2d13ae761e6630cccce5
XeliteXirish <shaunoneill7@gmail.com>
2020-02-08 01:24:46 +0000

    Basic

--- components/header/Login.jsx
+++ components/header/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Login = () => (
-    <a className="button loginButton is-outlined" title="Login to user profile">Login</a>
+    <a className="button loginButton is-outlined is-info" title="Login to user profile">Login</a>
 );
 
 export default Login;
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 import Link from 'next/link'
 import Login from "../header/Login";
 
-const Navbar = ({className}) => {
+const Navbar = () => {
 
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const toggleDropdown = useCallback(() => () => setIsDropdownOpen(!isDropdownOpen), [isDropdownOpen]);
 
     return (
-        <nav className={`${className} navbar is-fixed-top`}>
+        <nav className={`navbar`}>
             <div className="navbar-brand">
                 <Link href="/">
                     <a className={`${isDropdownOpen ? 'is-active' : ''} navbar-item navbar-title`}>
-                        PlaceHolderNameHere
+                        <b>GetOnTrack</b>
                     </a>
                 </Link>
 
@@ -26,7 +26,7 @@ const Navbar = ({className}) => {
             </div>
 
             <div className="navbar-menu" id="nav-bar">
-                <div className="navbar-end">
+                <div className="navbar-start">
                     <Link href="/timetables">
                         <a className="navbar-item">
                             Timetables
@@ -39,6 +39,9 @@ const Navbar = ({className}) => {
                             </a>
                         </Link>
                     </div>
+
+                </div>
+                <div className="navbar-end">
                     <div className="navbar-item">
                         <Login/>
                     </div>
