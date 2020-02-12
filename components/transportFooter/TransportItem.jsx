05279bf4258cb5dcd9bb219264f5ef13ebf96b1d
XeliteXirish <shaunoneill7@gmail.com>
2020-02-12 01:48:37 +0000

    Fix icon

--- components/transportFooter/TransportItem.jsx
+++ components/transportFooter/TransportItem.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {faChevronCircleRight} from "@fortawesome/free-solid-svg-icons";
 
 const TransportItem = ({type, icon, color}) => (
     <a className="column transportItem has-text-centered" style={{backgroundColor: color}}>
         <div className="columns is-vcentered is-gapless">
             <div className="column is-3">
-                <img src={icon}/>
+                <img src={icon} alt={type}/>
             </div>
             <div className="column">
                 <b>{type}</b><br/>
                 <span>2 stops </span>
             </div>
             <div className="column is-2">
-                <FontAwesomeIcon icon={faSearch}/>
+                <FontAwesomeIcon icon={faChevronCircleRight}/>
             </div>
         </div>
     </a>
