9b470e97bc0888f300a8773bf3d8c84c3701f5fd
XeliteXirish <shaunoneill7@gmail.com>
2020-02-12 01:46:24 +0000

    Split into components

---
+++ components/transportFooter/TransportFooter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import TransportItem from "./TransportItem";
+
+import busIcon from "../../assets/img/bus.svg";
+import railIcon from "../../assets/img/rail.svg";
+import luasIcon from "../../assets/img/luas.svg";
+import bikeIcon from "../../assets/img/bike.svg";
+
+const TransportFooter = () => (
+    <div className="transportFooter is-hidden-mobile">
+        <span className="footerTitle"><b>CHOOSE </b>TRANSPORT:</span>
+        <div className="columns is-centered">
+            <TransportItem type="Dublin Bus" icon={busIcon} color="#FFCC01"/>
+            <TransportItem type="Irish Rail" icon={railIcon} color="#00713D"/>
+            <TransportItem type="Luas" icon={luasIcon} color="#542F92"/>
+            <TransportItem type="Dublin Bikes" icon={bikeIcon} color="#114D57"/>
+        </div>
+    </div>
+);
+
+export default TransportFooter;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import DefaultLayout from "../layouts/default";
-import TransportItem from "../components/TransportItem";
 import GoogleMap from '../components/map/GoogleMap';
-// Import Icons here for now, will move to component later
-import busIcon from '../assets/img/bus.svg';
-import railIcon from '../assets/img/rail.svg';
-import luasIcon from '../assets/img/luas.svg';
-import bikeIcon from '../assets/img/bike.svg';
+import TransportFooter from "../components/transportFooter/TransportFooter";
 
 const Index = () => (
     <DefaultLayout title="Home">
@@ -14,11 +9,11 @@ const Index = () => (
             <div className="column columnInfo">
                 <h1 className="title is-1">
                         <span className="header-title">
-                            Welcome to <b>GetOnTrack</b>
+                            Welcome to <br/><b>GetOnTrack</b>
                         </span>
                 </h1>
-                <h2 className="is-5 subtitle">
-                    One stop solution to move and commute around Dublin
+                <h2 className="is-4 subtitle">
+                    Your one stop solution to move and commute around Dublin
                 </h2>
                 <div className="fancySearchBox control has-icons-right">
                     <input className="input is-large" type="text" placeholder="Where do you want to go?"/>
@@ -30,16 +25,7 @@ const Index = () => (
             <div className="column is-three-fifths is-hidden-mobile columnMap">
                 <GoogleMap/>
             </div>
-
-            <div className="transportFooter is-hidden-mobile">
-                <b>CHOOSE TRANSPORT</b>
-                <div className="columns is-centered">
-                    <TransportItem type="Dublin Bus" icon={busIcon} color="#FFCC01"/>
-                    <TransportItem type="Irish Rail" icon={railIcon} color="#00713D"/>
-                    <TransportItem type="Luas" icon={luasIcon} color="#542F92"/>
-                    <TransportItem type="Dublin Bikes" icon={bikeIcon} color="#114D57"/>
-                </div>
-            </div>
+            <TransportFooter/>
         </div>
     </DefaultLayout>
 );
