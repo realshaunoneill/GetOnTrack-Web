5380f85a97e11ed67f6a09db3649094abc6f7435
XeliteXirish <shaunoneill7@gmail.com>
2020-02-14 13:34:23 +0000

    Fake results

--- components/realTime/RealTimeResults.jsx
+++ components/realTime/RealTimeResults.jsx
@@ -4,10 +4,10 @@ import RealTimeResultItem from "./RealTimeResultItem";
 const RealTimeResults = ({type, results}) => (
     <div className="realTimeResults">
         <h1><b>{type}</b> Results:</h1>
-        <RealTimeResultItem type={type} number={120} name="Dublin" departure="18:00" arrival="19:00" late="6 mins"/>
-        <RealTimeResultItem type={type} number={120} name="Dublin" departure="18:00" arrival="19:00" late="6 mins"/>
-        <RealTimeResultItem type={type} number={120} name="Dublin" departure="18:00" arrival="19:00" late="6 mins"/>
-        <RealTimeResultItem type={type} number={120} name="Dublin" departure="18:00" arrival="19:00" late="6 mins"/>
+        {results.map(({number, name, departure, arrival, late}) => (
+            <RealTimeResultItem type={type} number={number} name={name} departure={departure} arrival={arrival}
+                                late={late}/>
+        ))};
     </div>
 );
 
--- pages/realtime/dublinbus.jsx
+++ pages/realtime/dublinbus.jsx
@@ -4,9 +4,16 @@ import RealTime from "../../components/realTime/RealTime";
 
 import busIcon from "../../assets/img/bus.svg";
 
+const fakeResults = [
+    {number: 120, name: "Cork", departure: "18:00", arrival: "19:00", late: "6 mins"},
+    {number: 120, name: "Cork", departure: "18:00", arrival: "19:00", late: "6 mins"},
+    {number: 120, name: "Cork", departure: "18:00", arrival: "19:00", late: "6 mins"},
+    {number: 120, name: "Cork", departure: "18:00", arrival: "19:00", late: "6 mins"},
+];
+
 const DublinBus = () => (
     <DefaultLayout title="Search Results">
-        <RealTime type="Dublin Bus" colour={"#FFCC01"} icon={busIcon}/>
+        <RealTime type="Dublin Bus" colour={"#FFCC01"} icon={busIcon} results={fakeResults}/>
     </DefaultLayout>
 );
 
