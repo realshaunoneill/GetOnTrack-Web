7f3b79c278ee9e2e77c4b37c903f4817edd7cc3f
XeliteXirish <shaunoneill7@gmail.com>
2020-02-16 01:49:21 +0000

    Real time page

--- components/realTime/RealTimeResults.jsx
+++ components/realTime/RealTimeResults.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import RealTimeResultItem from "./RealTimeResultItem";
 
 const displayResults = (type, results) => {
-    if (results.length === 0 || !results) return <span>Loading</span>
+    if (results.length === 0 || !Array.isArray(results)) return <span>Loading</span>;
     return results.map(({number, name, departure, arrival, late}) => (
         <RealTimeResultItem type={type} number={number} name={name} departure={departure} arrival={arrival}
                             late={late}/>
