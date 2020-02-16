f42aa48523b816a3a8a55f177cb712370b3cf111
XeliteXirish <shaunoneill7@gmail.com>
2020-02-16 22:02:19 +0000

    Loading spinner which I probably have to move soon

--- components/realTime/RealTimeResults.jsx
+++ components/realTime/RealTimeResults.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
+
 import RealTimeResultItem from "./RealTimeResultItem";
 
 const displayResults = (type, results) => {
-    if (results.length === 0 || !Array.isArray(results)) return <span>Loading</span>;
+    if (results.length === 0 || !Array.isArray(results)) return <Loader
+        className="loading has-text-centered"
+        type="Puff"
+        color="#000000"
+        height={"15rem"}
+        width={"15rem"}
+    />;
     return results.map(({number, name, departure, arrival, late}) => (
         <RealTimeResultItem type={type} number={number} name={name} departure={departure} arrival={arrival}
                             late={late}/>
