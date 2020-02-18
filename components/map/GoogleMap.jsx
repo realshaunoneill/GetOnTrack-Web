0361ea17cbfcf3d54949e6ba60fdafe47c266302
XeliteXirish <shaunoneill7@gmail.com>
2020-02-18 12:01:37 +0000

    Map location hook

--- components/map/GoogleMap.jsx
+++ components/map/GoogleMap.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import {usePosition} from 'use-position';
 
 const AnyReactComponent = ({ text }) => (
     <div style={{
@@ -17,19 +18,21 @@ const AnyReactComponent = ({ text }) => (
     </div>
 );
 
-const GoogleMap = ({center = {"lat": 53.35, "lng": -6.40}}) => (
-    <GoogleMapReact
-        bootstrapURLKeys={{key: "AIzaSyBk7cQco6GEd2AfR3Ybq5Ppd-Fs9zbBbG8"}}
-        defaultCenter={center}
-        defaultZoom={11}
-    >
-
-        <AnyReactComponent
-            lat={53.35}
-            lng={-6.40}
-            text="My Marker"
-        />
-    </GoogleMapReact>
-);
+const GoogleMap = ({center = {"lat": 53.35, "lng": -6.40}}) => {
+    const {latitude, longitude, timestamp, accuracy, error} = usePosition();
+    return (
+        <GoogleMapReact
+            bootstrapURLKeys={{key: "AIzaSyBk7cQco6GEd2AfR3Ybq5Ppd-Fs9zbBbG8"}}
+            defaultCenter={center}
+            defaultZoom={11}
+        >
+            <AnyReactComponent
+                lat={latitude}
+                lng={longitude}
+                text="Current Location"
+            />
+        </GoogleMapReact>
+    );
+};
 
 export default GoogleMap;
