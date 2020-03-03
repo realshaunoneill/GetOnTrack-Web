db27b0e05895bb4a497c50445780d0cc3c72de2e
XeliteXirish <shaunoneill7@gmail.com>
2020-03-03 00:56:13 +0000

    Dublin bus mock server

--- pages/realtime/dublinbus.jsx
+++ pages/realtime/dublinbus.jsx
@@ -5,6 +5,8 @@ import RealTime from "../../components/realTime/RealTime";
 import busIcon from "../../assets/img/bus.svg";
 import {DUBLIN_BUS_YELLOW} from '../../assets/Colours';
 
+const API_URL = (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://api.getontrack.ie');
+
 const DublinBus = ({query}) => {
     const [stopID, setStopID] = useState(query.stop);
     const [stopLocationName, setStopLocationName] = useState("...");
@@ -14,14 +16,14 @@ const DublinBus = ({query}) => {
     const fetchData = async (stopID) => {
         if (!stopID) return null;
 
-        const stopData = await (await fetch(`https://api.getontrack.ie/dublinbus/stops?stopid=${stopID}`)).json();
+        const stopData = await (await fetch(`${API_URL}/dublinbus/stops?stopid=${stopID}`)).json();
         console.debug({stopData});
         if (stopData.errorcode === "0" && stopData.results.length > 0) {
             setStopLocationName(stopData.results[0].shortname);
             setStopCoords({lat: stopData.results[0].latitude, lng: stopData.results[0].longitude});
         }
 
-        const realtimeData = await (await fetch(`https://api.getontrack.ie/dublinbus/live?stopid=${stopID}`)).json();
+        const realtimeData = await (await fetch(`${API_URL}/dublinbus/live?stopid=${stopID}`)).json();
         console.debug({realtimeData})
         if (realtimeData.errorcode === "0") {
             setApiResults(realtimeData.results)
