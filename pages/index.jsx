7be12ab57b893e423bd1d09e3767167c2b3491d9
XeliteXirish <shaunoneill7@gmail.com>
2020-03-31 04:11:25 +0100

    Login profile picture

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import DefaultLayout from '../layouts/default';
 
 import Header from '../components/header/Header';
 import FeaturesBar from '../components/featuresBar/FeaturesBar';
-import RealTimeList from '../components/realtimeList/RealtimeList';
+import RealTimeList from '../components/realTime/RealTimeList';
 
 const Index = () => (
   <DefaultLayout title="Home">
